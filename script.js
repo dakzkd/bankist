e30408b2b7fda6ef0cbf4382078679c1e2e8b17e
dakzkd <damir.kadyr@gmail.com>
2021-07-04 23:56:48 +0600

    event delegation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 ///////////////////////////////////////
 // Modal window
 
@@ -29,6 +30,54 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////////////////
+// Page Navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Add event listner to common parent element
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // 2. Determine what element originated the event
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// btnScroll
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+
+  console.log(e.target.getBoundingClientRect());
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // old way
+  // window,
+  //   scrollTo({
+  //     left: s1coords.left + window.pageXOffset,
+  //     top: s1coords.top + window.pageYOffset,
+  //     behavior: 'smooth',
+  //   });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -36,34 +85,78 @@ document.addEventListener('keydown', function (e) {
 
 ////////////////////////////////////////////////
 // Selecting elements
-console.log(document.documentElement);
-console.log(document.head);
-const header = document.querySelector('.header');
-const sectionsAll = document.querySelectorAll('.section');
-console.log(sectionsAll);
+// console.log(document.documentElement);
+// console.log(document.head);
+// const header = document.querySelector('.header');
+// const sectionsAll = document.querySelectorAll('.section');
+// console.log(sectionsAll);
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML =
-  'We use cookies for improved functionality and analitycs. <button class="btn btn--close--cookie">Got it!</button>';
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'We use cookies for improved functionality and analitycs. <button class="btn btn--close--cookie">Got it!</button>';
 
-header.prepend(message);
+// header.prepend(message);
 // header.append(message);
-document
-  .querySelector('.btn--close--cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// document
+//   .querySelector('.btn--close--cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
 
 // Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(getComputedStyle(message).height);
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+// console.log(getComputedStyle(message).height);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
 
 // document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 // Attributes
-const logo = document.querySelector('.nav__logo');
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+
+// Setting attributes
+// logo.alt = 'Beatufil minimlist logo';
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+//Classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+// Don't use logo.className = 'c' because it overwrites all classes
+
+// const h1 = document.querySelector('h1');
+// const alertH1 = e => {
+//   alert('addEventListner: Great!');
+
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
